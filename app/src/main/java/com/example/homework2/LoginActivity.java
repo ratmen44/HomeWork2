7c8e28cb4d5655dd35d09395e01ef0eff5530db5
Thanadon <ratmen44@gmail.com>
2019-08-24 22:55:34 +0700

    HomeWork22

---
+++ app/src/main/java/com/example/homework2/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.homework2;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast toast = Toast.makeText(LoginActivity.this, "Welcome to My ToDo application!", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
